0aed53074e2f7204cbb209de6b84b110d56d85ff
Wolfgang (Wolle) Ewald <wolfgang.ewald@wolles-elektronikkiste.de>
2022-09-17 15:31:39 +0200

    Add files via upload

--- examples/cool_car_basic_test/cool_car_basic_test.ino
+++ examples/cool_car_basic_test/cool_car_basic_test.ino
@@ -1,1 +1,25 @@
+#include <CoolCarLib.h>
 
+CoolCarBasic myCar = CoolCarBasic(7); // 7 passengers is maximum
+
+void setup() {
+  Serial.begin(9600);
+  
+  byte passengerLimit = myCar.getMaxPassengers();
+  unsigned int currentSpeed = myCar.getSpeed();
+  
+  Serial.print("Max. number of passengers: ");
+  Serial.println(passengerLimit);
+  
+  Serial.print("CurrentSpeed [km/h]: ");
+  Serial.println(currentSpeed);
+  
+  myCar.setSpeed(50);
+  currentSpeed = myCar.getSpeed();
+  Serial.print("CurrentSpeed [km/h]: ");
+  Serial.println(currentSpeed);
+  
+  myCar.hoot();  
+}
+
+void loop() {}
